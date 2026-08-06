7286dd1ec22a92a8be97252c9996cda504cfe7a3
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-08-07 01:27:36 +0530

    Create Greatest of two numbers.py

---
+++ Greatest of two numbers.py
@@ -0,0 +1,12 @@
+#Using if-else statement:
+num1,num2=20,10
+if num1>num2:
+    print(num1,"is great")
+else:
+    print(num2,"is great")
+#Using Ternary operator"
+num3,num4=20,10
+print((num3 if num3>num4 else num4))
+#Using inbuilt max() Function
+num5,num6=20,10
+print(max(num5,num6))
